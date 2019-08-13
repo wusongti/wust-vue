3d4770c74f40a046130c83e3c4d10cb93a8399e1
wust <wusongti@163.com>
2019-08-13 11:45:23 +0800

    优化

--- src/components/query/operation-log/operation-log-list.vue
+++ src/components/query/operation-log/operation-log-list.vue
@@ -5,93 +5,55 @@
   <div>
     <el-tabs>
       <el-tab-pane label="操作日志">
-          <form>
-            <div class="row">
-              <div class="col-xs-2 form-group">
-                <input type="text" class="form-control" placeholder="模块名" v-model="searchModel.moduleName"/>
-              </div>
-              <div class="col-xs-2 form-group">
-                <input type="text" class="form-control" placeholder="业务名" v-model="searchModel.businessName">
-              </div>
-              <div class="col-xs-2 form-group">
-                <select class="form-control"  v-model="searchModel.operationType">
-                  <option value="" selected>--请选择--</option>
-                  <option value="新增">新增</option>
-                  <option value="修改">修改</option>
-                  <option value="删除">删除</option>
-                  <option value="查询">查询</option>
-                  <option value="保存">保存</option>
-                  <option value="上传">上传</option>
-                  <option value="下载">下载</option>
-                  <option value="导入">导入</option>
-                  <option value="导出">导出</option>
-                  <option value="登录">登录</option>
-                  <option value="登出">登出</option>
-                </select>
-              </div>
-              <div>
-                <button class="btn btn-danger btn-sm" type="reset">重置</button>
-                <button class="btn btn-primary btn-sm" type="button" @click="search">查询</button>
-              </div>
-            </div>
-          </form>
-          <div class="panel-body progress-panel">
-            <div class="row">
-              <div class="btn-group pull-right btn-group-xs" role="group" aria-label="...">
-              </div>
-            </div>
-          </div>
-          <table class="table table-hover table-bordered">
-            <thead>
-            <tr>
-              <th>操作模块</th>
-              <th>操作业务</th>
-              <th>操作类型</th>
-              <th>操作角色</th>
-              <th>操作用户</th>
-              <th>操作时间</th>
-              <th>操作IP</th>
-              <th>来源</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr :key="data.id" v-for="data in baseDto.lstDto">
-              <td>{{data.moduleName}}</td>
-              <td>
-                {{data.businessName}}
-              </td>
-              <td>
-                {{data.operationType}}
-              </td>
-              <td>
-                {{data.operationRole}}
-              </td>
-              <td>
-                {{data.userRealName}}
-              </td>
-              <td>
-                {{data.createTime | formatDate('yyyy-MM-dd hh:mm:ss')}}
-              </td>
-              <td>
-                {{data.operationIp}}
-              </td>
-              <td>
-                {{data.source}}
-              </td>
-            </tr>
-            </tbody>
-            <tfoot>
-            <tr>
-              <td colspan="8">
-                <pagination-component v-bind:currentPage="searchModel.pageDto.currentPage"
-                                      v-bind:showCount="searchModel.pageDto.showCount"
-                                      v-bind:totalResult="baseDto.page.totalResult"
-                                      v-on:updatePageIndex="pageIndexChange"
-                                      @pageClick="listPage"></pagination-component>
-              </td>
-            </tr>
-            </tfoot>
-          </table>
+          <operation-log-search-bar v-on:search="search"></operation-log-search-bar>
+        <el-table
+          border
+          size="mini"
+          :data="baseDto.lstDto">
+          style="width: 100%">
+          <el-table-column
+            prop="moduleName"
+            label="模块"
+            width="200">
+          </el-table-column>
+          <el-table-column
+            prop="businessName"
+            label="业务"
+            width="200">
+          </el-table-column>
+          <el-table-column
+            prop="operationType"
+            label="操作类型"
+            width="100">
+          </el-table-column>
+          <el-table-column
+            prop="userRealName"
+            label="操作用户"
+            width="120">
+          </el-table-column>
+          <el-table-column
+            label="操作时间"
+            width="180">
+            <template slot-scope="scope">
+              <i class="el-icon-time"></i>
+              <span style="margin-left: 10px">{{ scope.row.createTime | formatDate('yyyy-MM-dd hh:mm:ss')}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="operationIp"
+            label="操作IP"
+            width="180">
+          </el-table-column>
+        </el-table>
+        <el-pagination
+          @size-change="pageSizeChange"
+          @current-change="pageIndexChange"
+          :current-page="searchModel.pageDto.currentPage"
+          :page-sizes="searchModel.pageDto.pageSizes"
+          :page-size="searchModel.pageDto.showCount"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="baseDto.page.totalResult">
+        </el-pagination>
       </el-tab-pane>
     </el-tabs>
   </div>
@@ -99,16 +61,20 @@
 <script>
 import Vue from 'vue'
 import PaginationComponent from '../../../common/component/pagination-component.vue'
+import OperationLogSearchBar from './operation-log-search-bar'
 
 export default {
   name: 'OprationLogList',
   components: {
+    OperationLogSearchBar,
     PaginationComponent
   },
   data () {
     return {
       searchModel: {
-        pageDto: {showCount: 10, currentPage: 1},
+        pageDto: {
+          showCount: 10, currentPage: 1, pageSizes: [10, 20, 30, 100]
+        },
         moduleName: '',
         businessName: '',
         operationType: ''
@@ -138,10 +104,22 @@ export default {
         }
       })
     },
+    pageSizeChange: function (e) {
+      this.searchModel.pageDto.showCount = e
+      this.listPage()
+    },
     pageIndexChange: function (e) {
       this.searchModel.pageDto.currentPage = e
+      this.listPage()
     },
-    search: function () {
+    search: function (p) {
+      if (!p.isCollapse) { // 非高级查询，则只需要根据关键字查询
+        if (!Vue.$isNullOrIsBlankOrIsUndefined(p.searchKey)) {
+          this.searchModel.moduleName = p.searchKey
+        }
+      } else {
+        this.searchModel = p.searchModel
+      }
       this.searchModel.pageDto.currentPage = 1
       this.listPage()
     }
---
+++ src/components/query/operation-log/operation-log-search-bar.vue
@@ -0,0 +1,73 @@
+<template xmlns:v-data-dictionary-directive="http://www.w3.org/1999/xhtml">
+  <div class="search">
+    <div style="margin-bottom: 5px">
+      <el-form :inline="true" label-width="150px"  size="small">
+        <el-form-item label="查询关键字">
+          <el-input size="small" v-model="searchKey">
+            <el-button slot="append" @click="toggleCollapse" :icon="isCollapse ? 'el-icon-remove-outline':'el-icon-circle-plus-outline'">高级</el-button>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" size="small" @click="search"><li class="el-icon-search">查询</li></el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <el-form :model="searchModel" label-width="150px"  size="small" v-bind:style="isCollapse ? 'display:block':'display:none'">
+      <el-form-item label="模块名">
+        <el-input v-model="searchModel.moduleName"></el-input>
+      </el-form-item>
+      <el-form-item label="业务名">
+        <el-input v-model="searchModel.businessName"></el-input>
+      </el-form-item>
+      <el-form-item label="操作类型">
+        <select class="form-control"  v-model="searchModel.operationType">
+          <option value="" selected>--请选择--</option>
+          <option value="新增">新增</option>
+          <option value="修改">修改</option>
+          <option value="删除">删除</option>
+          <option value="查询">查询</option>
+          <option value="保存">保存</option>
+          <option value="上传">上传</option>
+          <option value="下载">下载</option>
+          <option value="导入">导入</option>
+          <option value="导出">导出</option>
+          <option value="登录">登录</option>
+          <option value="登出">登出</option>
+        </select>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'operation-log-search-bar',
+  data () {
+    return {
+      isCollapse: false,
+      searchKey: '',
+      searchModel: {
+        pageDto: {
+          showCount: 10, currentPage: 1, pageSizes: [10, 20, 30, 100]
+        },
+        moduleName: '',
+        businessName: '',
+        operationType: ''
+      }
+    }
+  },
+  methods: {
+    toggleCollapse: function () {
+      this.isCollapse = !this.isCollapse
+    },
+    search: function () {
+      let p = {isCollapse: this.isCollapse, searchKey: this.searchKey, searchModel: this.searchModel}
+      this.$emit('search', p)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
