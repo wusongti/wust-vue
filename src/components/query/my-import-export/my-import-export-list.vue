973db452cb46386e2a6be4deed99d32285e61370
wust <wusongti@163.com>
2019-08-13 11:35:44 +0800

    优化

--- src/components/query/my-import-export/my-import-export-list.vue
+++ src/components/query/my-import-export/my-import-export-list.vue
@@ -1,84 +1,80 @@
 /**
 * Created by WST on 2019/5/23.
 */
-<template xmlns:v-data-dictionary-directive="http://www.w3.org/1999/xhtml">
+<template>
   <div>
     <el-tabs>
       <el-tab-pane label="我的导入导出">
-        <form>
-          <div class="row">
-            <div class="col-xs-2 form-group">
-              <select class="form-control" v-data-dictionary-directive:1005 v-model="searchModel.status">
-              </select>
-            </div>
-            <div>
-              <button class="btn btn-danger btn-sm" type="reset">重置</button>
-              <button class="btn btn-primary btn-sm" type="button" @click="search">查询</button>
-            </div>
-          </div>
-        </form>
-        <div class="panel-body progress-panel">
-          <div class="row">
-            <div class="btn-group pull-right btn-group-xs" role="group" aria-label="...">
-            </div>
-          </div>
-        </div>
-        <table class="table table-hover table-bordered">
-          <thead>
-          <tr>
-            <th>编号</th>
-            <th>模块名</th>
-            <th>操作类型</th>
-            <th>状态</th>
-            <th>描述</th>
-            <th width="200">开始时间</th>
-            <th width="200">结束时间</th>
-            <th width="80">创建人</th>
-            <th width="150">操作</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr :key="data.id" v-for="data in baseDto.lstDto">
-            <td>{{data.batchNo}}</td>
-            <td>
-              {{data.moduleName}}
-            </td>
-            <td>
-              {{data.operationTypeLabel}}
-            </td>
-            <td>
-              {{data.statusLabel}}
-            </td>
-            <td>
-              {{data.description}}
-            </td>
-            <td>
-              {{data.startTime | formatDate('yyyy-MM-dd hh:mm:ss')}}
-            </td>
-            <td>
-              {{data.endTime | formatDate('yyyy-MM-dd hh:mm:ss')}}
-            </td>
-            <td>
-              {{data.createrName}}
-            </td>
-            <td>
-              <button type="button" class="btn btn-link btn-xs" @click="viewLog(data.msg)" v-has-permission="'MyImportExportList.download'">查看日志</button>
-              <button type="button" class="btn btn-link btn-xs" @click="downloadExcel(data.batchNo)" v-has-permission="'MyImportExportList.download'" :disabled="data.operationType == '100601'">下载文件</button>
-            </td>
-          </tr>
-          </tbody>
-          <tfoot>
-          <tr>
-            <td colspan="12">
-              <pagination-component v-bind:currentPage="searchModel.pageDto.currentPage"
-                                    v-bind:showCount="searchModel.pageDto.showCount"
-                                    v-bind:totalResult="baseDto.page.totalResult"
-                                    v-on:updatePageIndex="pageIndexChange"
-                                    @pageClick="listPage"></pagination-component>
-            </td>
-          </tr>
-          </tfoot>
-        </table>
+       <my-import-export-search-bar v-on:search="search"></my-import-export-search-bar>
+        <el-table
+          border
+          size="mini"
+          :data="baseDto.lstDto">
+          style="width: 100%">
+          <el-table-column
+            prop="batchNo"
+            label="编码"
+            width="120">
+          </el-table-column>
+          <el-table-column
+            prop="moduleName"
+            label="模块名"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="operationTypeLabel"
+            label="操作类型"
+            width="100">
+          </el-table-column>
+          <el-table-column
+            prop="statusLabel"
+            label="状态"
+            width="100">
+          </el-table-column>
+          <el-table-column
+            prop="description"
+            label="描述"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="createrName"
+            label="创建人"
+            width="100">
+          </el-table-column>
+          <el-table-column
+            label="开始时间时间"
+            width="180">
+            <template slot-scope="scope">
+              <i class="el-icon-time"></i>
+              <span style="margin-left: 10px">{{ scope.row.startTime | formatDate('yyyy-MM-dd hh:mm:ss')}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="结束时间"
+            width="180">
+            <template slot-scope="scope">
+              <i class="el-icon-time"></i>
+              <span style="margin-left: 10px">{{ scope.row.endTime | formatDate('yyyy-MM-dd hh:mm:ss')}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="操作"
+            width="180">
+            <template slot-scope="scope">
+              <button type="button" class="btn btn-link btn-xs" @click="viewLog(scope.row.msg)" v-has-permission="'MyImportExportList.download'">查看日志</button>
+              <button type="button" class="btn btn-link btn-xs" @click="downloadExcel(scope.row.batchNo)" v-has-permission="'MyImportExportList.download'" :disabled="scope.row.operationType == '100601'">下载文件</button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-pagination
+          @size-change="pageSizeChange"
+          @current-change="pageIndexChange"
+          :current-page="searchModel.pageDto.currentPage"
+          :page-sizes="searchModel.pageDto.pageSizes"
+          :page-size="searchModel.pageDto.showCount"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="baseDto.page.totalResult">
+        </el-pagination>
       </el-tab-pane>
     </el-tabs>
     <el-dialog
@@ -97,16 +93,18 @@
 <script>
 import Vue from 'vue'
 import PaginationComponent from '../../../common/component/pagination-component.vue'
+import MyImportExportSearchBar from './my-import-export-search-bar'
 
 export default {
   name: 'ImportExportList',
   components: {
+    MyImportExportSearchBar,
     PaginationComponent
   },
   data () {
     return {
       searchModel: {
-        pageDto: {showCount: 10, currentPage: 1},
+        pageDto: {showCount: 10, currentPage: 1, pageSizes: [10, 20, 30, 100]},
         status: ''
       },
       baseDto: {page: {totalResult: 0}},
@@ -143,10 +141,22 @@ export default {
       this.logText = msg
       this.dialogVisible = true
     },
+    pageSizeChange: function (e) {
+      this.searchModel.pageDto.showCount = e
+      this.listPage()
+    },
     pageIndexChange: function (e) {
       this.searchModel.pageDto.currentPage = e
+      this.listPage()
     },
-    search: function () {
+    search: function (p) {
+      if (!p.isCollapse) { // 非高级查询，则只需要根据关键字查询
+        if (!Vue.$isNullOrIsBlankOrIsUndefined(p.searchKey)) {
+          this.searchModel.moduleName = p.searchKey
+        }
+      } else {
+        this.searchModel = p.searchModel
+      }
       this.searchModel.pageDto.currentPage = 1
       this.listPage()
     },
---
+++ src/components/query/my-import-export/my-import-export-search-bar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="search">
+    <div style="margin-bottom: 5px">
+      <el-form :inline="true" label-width="150px"  size="small">
+        <el-form-item label="查询关键字">
+          <el-input size="small" v-model="searchKey">
+            <el-button slot="append" @click="toggleCollapse" :icon="isCollapse ? 'el-icon-remove-outline':'el-icon-circle-plus-outline'">高级</el-button>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" size="small" @click="search"><li class="el-icon-search">查询</li></el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <el-form :model="searchModel" label-width="150px"  size="small" v-bind:style="isCollapse ? 'display:block':'display:none'">
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-import-export-search-bar',
+  data () {
+    return {
+      isCollapse: false,
+      searchKey: '',
+      searchModel: {
+        pageDto: {
+          showCount: 10, currentPage: 1, pageSizes: [10, 20, 30, 100]
+        },
+        name: '',
+        status: ''
+      }
+    }
+  },
+  methods: {
+    toggleCollapse: function () {
+      this.isCollapse = !this.isCollapse
+    },
+    search: function () {
+      let p = {isCollapse: this.isCollapse, searchKey: this.searchKey, searchModel: this.searchModel}
+      this.$emit('search', p)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
